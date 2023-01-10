abc5e4ebbef7673113e3d6d45928c175aa392033
Anna Atiskova <anna.atiskova@gmail.com>
2023-01-10 22:36:05 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+btnStop.disabled = true;
 
+btnStart.setAttribute(
+    "style",
+    "position: absolute; top: 45%; right: 50%; width: 100px; height: 50px; font-size: 25px;"
+  );
+  btnStop.setAttribute(
+    "style",
+    "position: absolute; top: 51%; right: 50%; width: 100px; height: 50px; font-size: 25px;"
+  );
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+
+
+  btnStart.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        let hexColor = getRandomHexColor();
+        document.body.style.backgroundColor = hexColor;;
+    }, 1000);
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+  });
+
+  btnStop.addEventListener("click", () => {
+    clearInterval(timerId);
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+  });
